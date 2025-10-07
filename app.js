538b9c76da6db73dbc008b0d518798c07857364e
mamun <mdmamunriyan361@gmail.com>
2025-10-07 23:59:37 +0600

    Kf

---
+++ app.js
@@ -0,0 +1,19 @@
+
+/*
+* Challenge Requirements: Ask the user for their name and store it in a variable.
+﻿﻿Ask the user for their total income using prompt ().
+﻿Ask the user how many expenses they have, then use a loop to collect expense amounts dynamically.
+﻿make sure to validate all the inputs
+﻿Perform all calculations (tax, balance, savings, financial status).
+﻿Show the results in the console.
+/*
+MI Personal Budget Tracker
+User: Alex
+Total Income: $5000
+Total Expenses: $2200
+Tax Deducted (10%): $500
+Net Income After Tax: $4500
+Remaining Balance: $2300
+Savings (20% of balance): $460
+Needs Improvement. Consider reducing expenses.
+*/
